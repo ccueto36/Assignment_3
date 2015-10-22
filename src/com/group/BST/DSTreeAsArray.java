fc041508bd041c78b9d32f0a3c0ffb7e3a262626
Miguel Espinal <epmiguel@users.noreply.github.com>
2015-10-21 21:45:08 -0400

    Veo Arrays
    Comentando.

--- src/com/group/BST/DSTreeAsArray.java
+++ src/com/group/BST/DSTreeAsArray.java
@@ -11,12 +11,12 @@ import com.group.Tools.Location;
  */
 
 /**
- *  This class takes a regular binary search tree and transform it into a 
+ *  This class takes a regular binary search tree and transform it into a
  * array like representation of the tree. It storages the tree in to arrays:
  * one containing the values and the other containing the connections in the Tree.
  * It also provides a "selectedLocation" property to identify what element its being
  * selected
- * @author Manuel Garcia
+ * @author Manuel Garcia    //adicto al array as I thought lol
  */
 
 public class DSTreeAsArray<T> {
@@ -25,16 +25,16 @@ public class DSTreeAsArray<T> {
     private ArrayLocation selectedLocation;
 
     /**
-     * 
+     *
      */
     public DSTreeAsArray(BSTree<T> tree) {
         selectedLocation = null; //No element selected
     }
 
     /**
-     * 
+     *
      * @return the array containing the connections between nodes. Each array
-     * value storages the location of the position he is pointing to. if null, 
+     * value storages the location of the position he is pointing to. if null,
      * means that it is not pointing to anyone
      */
     public Location[][] getConnectionsArray() {
@@ -42,7 +42,7 @@ public class DSTreeAsArray<T> {
     }
 
     /**
-    * 
+    *
     * @return the array containing the tree's values in a array-like form.
     * if null, means that there is no value to be shown
     */
@@ -51,8 +51,8 @@ public class DSTreeAsArray<T> {
     }
 
     /**
-     * 
-     * @return the selected position. if null, means that no value has been 
+     *
+     * @return the selected position. if null, means that no value has been
      * selected
      */
     public ArrayLocation getSelectedLocation() {
@@ -60,21 +60,21 @@ public class DSTreeAsArray<T> {
     }
 
     /**
-     * 
-     * @param selectedLocation sets the location of the selected value in the 
+     *
+     * @param selectedLocation sets the location of the selected value in the
      * array
      */
     public void setSelectedLocation(ArrayLocation selectedLocation) {
         this.selectedLocation = selectedLocation;
     }
-    
+
     /**
      * Clear the the selectedLocation.
      */
     public void clearSelectedLocation(){
         this.selectedLocation = null;
     }
-    
+
     private class ArrayLocation{
         private Location target;
         private Location caller;
@@ -83,23 +83,23 @@ public class DSTreeAsArray<T> {
             target = null;
             caller = null;
         }
-        
+
         public Location getTarget(){
             return this.target;
         }
-        
+
         public Location getCaller(){
             return this.caller;
         }
-        
+
         public void setTarget(Location location){
             this.target = location;
         }
-        
+
         public void setCaller(Location location){
             this.caller = location;
         }
-        
+
     }
-    
+
 }
