05482f91276d2f9825a790befa6457ad5ae650a9
Manuel <mgarciacruzz@gmail.com>
2015-10-26 21:57:05 -0400

    Corrections made in the layout

--- src/com/group/graphics/GUIJFrame.java
+++ src/com/group/graphics/GUIJFrame.java
@@ -10,6 +10,7 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.HeadlessException;
+import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -30,7 +31,7 @@ import javax.swing.JRadioButton;
 public class GUIJFrame extends JFrame {
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-    int frameWidth = (int) screenSize.getWidth() * 2 / 3;
+    int frameWidth = (int) screenSize.getWidth() * 2 / 6;
     int frameHeight = (int) screenSize.getHeight() * 2 / 3;
 
     
@@ -116,12 +117,12 @@ public class GUIJFrame extends JFrame {
 
         drawPanel.setPenColor(Color.BLACK);
         drawPanel.line(0, 0, 1, 1);
-        drawPanel.setCanvasSize(frameWidth * 2 / 5, frameHeight * 2 / 3);
+        drawPanel.setCanvasSize(frameWidth * 5/8, frameHeight * 2 / 3);
 
         //Tutorial frame location
         gbc_tutorialPanel.gridx = 1;
         gbc_tutorialPanel.gridy = 0;
-        tutorialPanel.setPreferredSize(new Dimension(frameWidth * 3 / 5, frameHeight * 2 / 3));
+        tutorialPanel.setPreferredSize(new Dimension(frameWidth * 3/8, frameHeight * 2 / 3));
         
         //ButtonJPanel Location
         gbc_buttonPanel.gridx = 0;
@@ -135,31 +136,44 @@ public class GUIJFrame extends JFrame {
         
         gbc_Jrbtn_1.gridx = 0;
         gbc_Jrbtn_1.gridy = 0;
+        gbc_Jrbtn_1.weightx = 0.05;
+        gbc_Jrbtn_1.anchor = GridBagConstraints.WEST;
         buttonPanel.add(jbtChoice1,gbc_Jrbtn_1);
         
         gbc_Jrbtn_2.gridx = 1 ;
         gbc_Jrbtn_2.gridy = 0 ;
+        gbc_Jrbtn_2.weightx = 0.05;
+        gbc_Jrbtn_2.anchor = GridBagConstraints.WEST;
         buttonPanel.add(jbtChoice2, gbc_Jrbtn_2) ;
         
         gbc_Jrbtn_3.gridx = 2 ;
         gbc_Jrbtn_3.gridy = 0 ;
+        gbc_Jrbtn_3.weightx = 0.05;
+        gbc_Jrbtn_3.anchor = GridBagConstraints.WEST;
         buttonPanel.add(jbtChoice3, gbc_Jrbtn_3) ;
         
         gbc_Jrbtn_4.gridx = 3 ;
         gbc_Jrbtn_4.gridy = 0 ;
+        gbc_Jrbtn_4.weightx = 0.90;
+        gbc_Jrbtn_4.anchor = GridBagConstraints.WEST;
         buttonPanel.add(jbtChoice4, gbc_Jrbtn_4) ;
         
-        gbc_Jrbtn_1.gridx = 0 ;
-        gbc_Jrbtn_1.gridy = 1 ;
-        buttonPanel.add(btPrev, gbc_Jrbtn_1) ;
+        gbc_Btn_Prev.gridx = 0 ;
+        gbc_Btn_Prev.gridy = 1 ;
+        gbc_Btn_Prev.insets = new Insets(10,0,0,0);
+        gbc_Btn_Prev.anchor = GridBagConstraints.WEST;
+        buttonPanel.add(btPrev, gbc_Btn_Prev) ;
         
-        gbc_Jrbtn_2.gridx = 1 ; 
-        gbc_Jrbtn_2.gridy = 1 ;
-        buttonPanel.add(btSubmit, gbc_Jrbtn_2) ;
+        gbc_Btn_Submit.gridx = 1 ; 
+        gbc_Btn_Submit.gridy = 1 ;
+        gbc_Btn_Submit.insets = new Insets(10,0,0,0);
+        buttonPanel.add(btSubmit, gbc_Btn_Submit) ;
         
-        gbc_Jrbtn_4.gridx = 4 ;
-        gbc_Jrbtn_4.gridy = 1 ;
-        buttonPanel.add(btNext, gbc_Jrbtn_4) ;
+        gbc_Btn_Next.gridx = 4 ;
+        gbc_Btn_Next.gridy = 1 ;
+        gbc_Btn_Next.insets = new Insets(10,0,0,0);
+        gbc_Btn_Next.anchor = GridBagConstraints.EAST;
+        buttonPanel.add(btNext, gbc_Btn_Next) ;
         
         /***********************************************************************
         * Puts radioButtons in a group
